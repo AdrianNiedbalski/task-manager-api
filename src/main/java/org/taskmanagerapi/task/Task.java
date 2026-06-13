b4481da1215060d0ef15a6e0b6413d865579f03c
User <niedbalskiadrian@gmail.com>
2026-06-13 19:20:23 +0200

    Add task repository

--- src/main/java/org/taskmanagerapi/task/Task.java
+++ src/main/java/org/taskmanagerapi/task/Task.java
@@ -1,31 +1,29 @@
 package org.taskmanagerapi.task;
 
 import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
 @Entity
 public class Task {
-    protected Task (){}
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+    private LocalDateTime createdAt;
 
-    public Task (String title, String description) {
+    public Task(String title, String description) {
         this.title = title;
         this.description = description;
         this.status = TaskStatus.TODO;
         this.createdAt = LocalDateTime.now();
     }
 
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    private String title;
-
-    private String description;
-
-    @Enumerated(EnumType.STRING)
-    private TaskStatus status;
-
-    private LocalDateTime createdAt;
+    protected Task() {
+    }
 
     public Long getId() {
         return id;
