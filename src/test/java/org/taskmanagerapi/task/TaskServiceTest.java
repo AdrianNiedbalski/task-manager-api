9116d3578382a59fcfb5f240d217bcce2d13836c
User <niedbalskiadrian@gmail.com>
2026-06-13 23:20:13 +0200

    Fix create task service test

--- src/test/java/org/taskmanagerapi/task/TaskServiceTest.java
+++ src/test/java/org/taskmanagerapi/task/TaskServiceTest.java
@@ -1,14 +1,15 @@
 package org.taskmanagerapi.task;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,21 +21,19 @@ class TaskServiceTest {
     @InjectMocks
     private TaskService taskService;
 
-    @BeforeEach
-    void setUp() {
-    }
-
-
+    @Test
     @DisplayName("Create Task Test")
     void createTaskTest() {
         String title = "Test task";
         String description = "Test task description";
         Task task = taskService.create(title, description);
 
-        when(taskService.create(title, description)).thenReturn(task);
+        when(taskRepository.save(task)).thenReturn(task);
 
-        assertNotNull(task);
+        assertNotNull(taskService.create(title, description));
         assertEquals(title, task.getTitle());
         assertEquals(description, task.getDescription());
+
+        verify(taskRepository.save(any(Task.class)));
     }
 }
