7d208cd73572cde17afe5c83440ae10c0b8bd099
User <niedbalskiadrian@gmail.com>
2026-06-13 23:12:55 +0200

    Create task test

---
+++ src/test/java/org/taskmanagerapi/task/TaskServiceTest.java
@@ -0,0 +1,38 @@
+package org.taskmanagerapi.task;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("TaskService Test")
+class TaskServiceTest {
+    @Mock
+    private TaskRepository taskRepository;
+
+    @InjectMocks
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+    @DisplayName("Create Task Test")
+    void createTaskTest() {
+        String title = "Test task";
+        String description = "Test task description";
+
+        Task task = taskService.create(title, description);
+
+        assertNotNull(task);
+        assertEquals(title, task.getTitle());
+        assertEquals(description, task.getDescription());
+    }
+}
