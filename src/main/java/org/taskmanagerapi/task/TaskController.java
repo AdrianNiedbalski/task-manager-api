e95b2439469d4afc160907844661a05837f83403
User <niedbalskiadrian@gmail.com>
2026-06-14 10:29:13 +0200

    Controller fix

--- src/main/java/org/taskmanagerapi/task/TaskController.java
+++ src/main/java/org/taskmanagerapi/task/TaskController.java
@@ -1,12 +1,12 @@
 package org.taskmanagerapi.task;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 import org.taskmanagerapi.task.dto.CreateTaskResponse;
+import org.taskmanagerapi.task.dto.TaskResponse;
 
 @RestController
-@RequestMapping("api/tasks")
+@RequestMapping("/api/tasks")
 public class TaskController {
     private final TaskService taskService;
 
@@ -14,9 +14,20 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    @PostMapping("api/tasks/create")
+    @PostMapping("/create")
+    @Valid
+    @RequestBody
     public CreateTaskResponse createTask(String title, String description) {
         Task createTask = taskService.create(title, description);
-        return new CreateTaskResponse(createTask.getTitle(), createTask.getDescription());
+        return new CreateTaskResponse(request.getTitle(), request.getDescription());
+    }
+
+    @GetMapping("/all")
+    public Task findAll() {
+        return taskService.findAll();
+    }
+
+    public TaskResponse toResponse(Task task) {
+        return new TaskResponse(task.getId(), task.getTitle(), task.getDescription(), task.getStatus());
     }
 }
