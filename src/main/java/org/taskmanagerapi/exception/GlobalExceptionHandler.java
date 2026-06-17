92b46d1aca4473a9a6b1381fc9fdc6586386e158
User <niedbalskiadrian@gmail.com>
2026-06-17 21:34:49 +0200

    GlobalExceptionHandler fix

--- src/main/java/org/taskmanagerapi/exception/GlobalExceptionHandler.java
+++ src/main/java/org/taskmanagerapi/exception/GlobalExceptionHandler.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.taskmanagerapi.task.exception.TaskNotFoundException;
 
-import java.util.Map;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaskNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse TaskNotFoundException (TaskNotFoundException e) {
+    public ErrorResponse handleTaskNotFound (TaskNotFoundException e) {
         return new ErrorResponse(e.getMessage());
     }
 }
